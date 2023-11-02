e6708e03a33e8131ce24a66256e9d6f3d0e1fd30
Patrick Schröder <patrick.schroeder@jambit.com>
2023-11-02 12:49:14 +0100

    refactor package and add properties to model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { PrintableListComponent } from './printable-list/printable-list.component';
 import { PrintableFormComponent } from './printable-form/printable-form.component';
 import {FormsModule} from "@angular/forms";
-import { FilamentFormComponent } from './filament-form/filament-form.component';
-import { FilamentListComponent } from './filament-list/filament-list.component';
+import { FilamentFormComponent } from './filament/filament-form/filament-form.component';
+import { FilamentListComponent } from './filament/filament-list/filament-list.component';
 
 @NgModule({
   declarations: [
--- src/app/models/filament.model.ts
+++ src/app/models/filament.model.ts
@@ -1,2 +1,11 @@
 export interface Filament {
+  id: number
+  name: string
+  manufacturer: string
+  material: string
+  color: string
+  weight: number
+  price: number
+  density: number
+
 }
