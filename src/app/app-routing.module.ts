5098f7a33050f4c37b2c4967732ca0cd5b890dba
Patrick Schröder <patrick.schroeder@jambit.com>
2023-11-02 14:12:44 +0100

    add simple list of filaments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PrintableListComponent} from "./printable-list/printable-list.component";
+import {FilamentListComponent} from "./filament/filament-list/filament-list.component";
 
 const routes: Routes = [
-  {path:"", component: PrintableListComponent}
+  {path:"", component: PrintableListComponent},
+  {path:"filament", component: FilamentListComponent}
 ];
 
 @NgModule({
--- src/app/models/printable.model.ts
+++ src/app/models/printable.model.ts
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+import {Filament} from "./filament.model";
+
 export interface Printable {
   id: number
   name: string
@@ -10,19 +11,6 @@ export interface Printable {
   filamentLength: number
   filamentCosts: number
   powerCosts: number
-=======
-import {Filament} from "./filament.model";
-
-export interface Printable {
-  id: number
-  name: string
-  gCode_filename: string
-  duration: number
-  filament_weight: number
-  filament_length: number
-  filament_costs: number
-  power_costs: number
   filament: Filament
->>>>>>> 20643e8 (establish filament model with form and list component)
 
 }
